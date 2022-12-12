097ef3cf6c31b107bc777201692e092381049e18
Wasabi <sbogomazov01@gmail.com>
2022-12-13 00:03:56 +0300

    tryPydantic

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
 from fastapi import FastAPI
+from schemas import Book
 
 app = FastAPI()
 
+users = []
+books = []
+
+
 @app.get('/')
 def root():
-    return {"key":"Hello Net"}
+    return {"key": "Im root"}
+
+
+@app.get('/users')
+def get_users():
+    return {
+        "users": " ".join(users)
+    }
+
+
+@app.get('/user/{user_id}/items/{item_id}/')
+def get_user_item(user_id: int, item_id: int):
+    return {
+        "user": user_id, "item": item_id
+    }
+
+
+@app.get('/books')
+def get_books():
+    return books
+
+
+@app.post('/books')
+def create_book(book: Book):
+    books.append(book)
+    return {
+        "result": "ok"
+    }
